09204cec985ed5c28e8c4338a7d4144124d6519f
青芥 <54880754@qq.com>
2023-10-31 22:12:30 +0800

    点击菜单跳转路由

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,12 @@ import { createBrowserRouter } from 'react-router-dom'
 import Layout from '@/pages/Layout'
 import Login from '@/pages/Login'
 import { AuthRoute } from '@/components/AuthRoute'
+
+// 导入二级路由
+import Home from '@/pages/Home'
+import Article from '@/pages/Article'
+import Publish from '@/pages/Publish'
+
 export default createBrowserRouter([
   {
     path: '/',
@@ -9,7 +15,12 @@ export default createBrowserRouter([
       <AuthRoute>
         <Layout />
       </AuthRoute>
-    )
+    ),
+    children: [
+      { index: true, element: <Home></Home> },
+      { path: 'article', element: <Article></Article> },
+      { path: 'publish', element: <Publish></Publish> }
+    ]
   },
   {
     path: '/Login',
