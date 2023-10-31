18736714d7092c594b2f184edbe4f29b0f780e60
青芥 <54880754@qq.com>
2023-10-31 12:00:59 +0800

    token控制路由权限

--- src/pages/Layout/index.js
+++ src/pages/Layout/index.js
@@ -1,9 +1,3 @@
-import { request } from '@/utils'
-import { useEffect } from 'react'
-
 export default function Layout() {
-  useEffect(() => {
-    request.get('/user/profile')
-  }, [])
   return <div>this is Layout</div>
 }
