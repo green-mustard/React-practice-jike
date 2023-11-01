7ab1fa61dd50ff682ee065b61d4ed6efe44b1a88
青芥 <54880754@qq.com>
2023-11-01 19:56:11 +0800

    echart组件的封装

---
+++ src/components/BarChart.js
@@ -0,0 +1,42 @@
+// 柱状图组件
+import * as echarts from 'echarts'
+import { useEffect, useRef } from 'react'
+
+const BarChart = ({ title, data }) => {
+  // 1. 把功能代码都放到这个组件中
+  const chartRef = useRef(null)
+  // 保证dom可用，再进行图表的渲染
+  useEffect(() => {
+    // 1. 获取渲染图表的dom节点
+    const chartDom = chartRef.current
+    // 2. 初始化echarts，生成图表实例对象
+    const myChart = echarts.init(chartDom)
+    // 3. 配置图表参数
+    const option = {
+      title: {
+        text: title
+      },
+      xAxis: {
+        type: 'category',
+        data: ['Vue', 'Angular', 'React']
+      },
+      yAxis: {
+        type: 'value'
+      },
+      series: [
+        {
+          data: data,
+          type: 'bar'
+        }
+      ]
+    }
+
+    // 4. 使用图表参数，完成图表的渲染
+    option && myChart.setOption(option)
+  }, [])
+  return <div ref={chartRef} style={{ width: '500px', height: '400px' }}></div>
+}
+
+// 2. 把可变的部分抽象成props参数
+
+export default BarChart
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,3 +1,12 @@
+import BarChart from '@/components/BarChart'
+
 export default function Home() {
-  return <div>this is Home</div>
+  const data1 = [100, 60, 30]
+  const data2 = [120, 80, 200]
+  return (
+    <div>
+      <BarChart title={'三大框架满意度'} data={data1} />
+      <BarChart title={'三大框架使用度'} data={data2} />
+    </div>
+  )
 }
