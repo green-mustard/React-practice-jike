42f6ae2ae041d9e14ffd62f25e0867e32b68ce86
青芥 <54880754@qq.com>
2023-11-01 14:38:58 +0800

    token的失效处理

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,6 +1,7 @@
 // axios的封装处理
 import axios from 'axios'
-import { getToken } from './token'
+import { getToken, removeToken } from './token'
+import router from '@/router'
 
 // 1、根域名的配置
 const request = axios.create({
@@ -39,6 +40,15 @@ request.interceptors.response.use(
   error => {
     // 超出 2xx 范围的状态码都会触发该函数。
     // 对响应错误做点什么
+
+    // 监控401 token失效
+    // console.dir(error)
+    // console.log(error.response.status)
+    if (error.response.status === 401) {
+      removeToken()
+      router.navigate('/login')
+      window.location.reload()
+    }
     return Promise.reject(error)
   }
 )
