4e28043fe38ba2b8d29e764867dbb1b69fb44ec9
青芥 <54880754@qq.com>
2023-11-08 09:52:41 +0800

    文章列表-携带id跳转文章编辑页

--- src/pages/Article/index.js
+++ src/pages/Article/index.js
@@ -1,5 +1,5 @@
 // 引入筛选区资源
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 import {
   Card,
   Breadcrumb,
@@ -28,6 +28,7 @@ const { RangePicker } = DatePicker
 
 const Article = () => {
   const { channelList } = useChannel()
+  const navigate = useNavigate()
 
   // 定义状态枚举
   const status = {
@@ -80,7 +81,12 @@ const Article = () => {
       render: data => {
         return (
           <Space size="middle">
-            <Button type="primary" shape="circle" icon={<EditOutlined />} />
+            <Button
+              type="primary"
+              shape="circle"
+              icon={<EditOutlined />}
+              onClick={() => navigate(`/publish?id=${data.id}`)}
+            />
             <Popconfirm
               title="Delete the task"
               description="Are you sure to delete this task?"
